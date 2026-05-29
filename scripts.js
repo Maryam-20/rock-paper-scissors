58655eeece7e0c29b2edd2cca40be189b49e38df
Maryam Anileleye <maryamanileleye@gmail.com>
2026-05-29 20:08:56 +0100

    Add UI
    Added interactive UI with button to play the game

--- scripts.js
+++ scripts.js
@@ -1,63 +1,122 @@
 let humanScore = 0;
 let computerScore = 0;
-round = 1
+const main = document.querySelector("main");
+
 
 function getComputerChoice () {
-    let choices = ["rock", "paper", "scissors"]
-    let choice_index = Math.floor(Math.random() * choices.length);
-    // console.log(choice_index)
-    let computer_choice = choices[choice_index]
-    // console.log("COMPUTER CHOICE: ",computer_choice)
-    return computer_choice.toLowerCase();
-}
+    return new Promise((resolve, reject) =>{
+        let choices = ["rock", "paper", "scissors"]
+        let choice_index = Math.floor(Math.random() * choices.length);
+        
+        let computer_choice = choices[choice_index]
+        
+        if (computer_choice) {
+            resolve(computer_choice);
+            
+        } else {
+            reject("FAILED TASK")
+        }
+        
+    })
+   
+};
 
 function getHumanChoice() {
-    let human_choice = prompt(`Rock, paper, scissor? Pick your choice.`)
-    // console.log("YOUR CHOICE",human_choice);
-    return human_choice.toLowerCase();
-}
+    return new Promise((resolve) => {
+        const human_choice = document.createElement("input");
+        human_choice.type = "text";
+        human_choice.placeholder = "Enter rock, paper or scissors"
+        human_choice.addEventListener('keydown', (e) => {
+            if (e.key === "Enter"){
+                const value = human_choice.value;
+                human_choice.remove();
 
-function playGame(){
-    for (let round =1; round<=5; round++){
-        if (round == 1){
-            console.log("Welcome to Rock, Paper Scissors Game! You are playing 5 rounds")
+                resolve(value)
             
+            }
             
-        }
-        console.log(`/Play Round ${round}`)
+        })
+        main.appendChild(human_choice);
+    })
+};
+
+
+const playRound = async function (round) {
+    if (round === 1) {
+        const welcomeDiv = document.createElement("div");
+        welcomeDiv.textContent = "Welcome to Rock, Paper, Scissors! You are playing 5 rounds";
         
-        const playRound = function () {
-                computer_choice_input = getComputerChoice()
-                human_choice_input = getHumanChoice()
-                if  (human_choice_input === computer_choice_input) {
-                    humanScore++
-                    console.log(`You Won this round! ${humanScore} : ${computerScore}`)
-                }else{
-                    computerScore++
-                    console.log(`You Loose this round ${humanScore} : ${computerScore}`)
-                }
-            };
-            playRound()
-            if (round < 5) {
-                continue
-            }else{
-                if (humanScore > computerScore) {
-                    console.log("You Won this Game!")
+        main.appendChild(welcomeDiv)
+    };
+    const numberRounds = document.createElement("h4")
+    numberRounds.textContent = `Round ${round}`;
+    main.appendChild(numberRounds)
 
-                }else {
-                    console.log("You Loose this Game!")
-                }
-            };
+    computer_choice_input = await getComputerChoice()
+    console.log(computer_choice_input)
+    human_choice_input = await getHumanChoice()
+    console.log(human_choice_input)
+    if  (human_choice_input === computer_choice_input) {
+        humanScore++
+        console.log(`You Won this round! ${humanScore} : ${computerScore}`)
+        const display = document.createElement("span")
+        display.textContent = ` - You Won ${humanScore} : ${computerScore}`
+        numberRounds.appendChild(display)
+    }else{
+        computerScore++
+        console.log(`You Loose this round ${humanScore} : ${computerScore}`)
+
+        const display = document.createElement("span")
+        display.textContent = ` - You Loose ${humanScore} : ${computerScore}`
+        numberRounds.appendChild(display)
+    }
+    
+};
+
+
+const play_game = document.querySelector("button")
+play_game.addEventListener('click', async () => {
+     play_game.disabled = true;
+    for (let round = 1; round <= 5; round++) {
         
+        await playRound(round)
+    };
+    
+    if (humanScore > computerScore) {
 
-    }      
-}
-playGame()
+        console.log("You Won this Game!");
+        const displayGameWinner = document.createElement("h2");
+        displayGameWinner.textContent = 'You Won this Game! \\ Game Finished';
+        main.appendChild(displayGameWinner);
 
+    } else if (computerScore > humanScore) {
 
+        console.log("You Lose this Game!");
+        const displayGameWinner = document.createElement("h2");
+        displayGameWinner.textContent = 'You Lose this Game! \\ Game Finished';
+        main.appendChild(displayGameWinner)
 
+    } else {
 
+        console.log("It's a Tie!");
+        const displayGameWinner = document.createElement("h2");
+        displayGameWinner.textContent = "It's a Tie! \\ Game Finished";
+        main.appendChild(displayGameWinner)
 
+    }
 
+    setTimeout(() => {
+        const childrenArray = Array.from(main.children);
+        const otherChildren = childrenArray.slice(2)
+        otherChildren.forEach(child => {
+           
+            child.remove()
+            
+            play_game.disabled = false;
+        })
+    }, 4000)
+    
+    
+    console.log("Game Finished")
+});
 
-console.log("Maryam Anileleye")
